3ea05d2e620d88f252a8973cba72fb9bc79e71d2
Blulogix <mohammadnemer1996@hotmail.com>
2019-07-29 17:45:15 +0300

    second commit\

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { Header } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardInfoComponent } from './dashboard/card-info/card-info.component'
+import { Dashboard } from './dashboard/dashboard.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,Header, FooterComponent, CardInfoComponent,Dashboard,CardInfoComponent
   ],
   imports: [
     BrowserModule
